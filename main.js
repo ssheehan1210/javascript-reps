d34ad4c3dbbfab0a8dd22206cb080595a2f8275a
Sam Sheehan <samsheehan@Sams-MacBook-Pro.local>
2017-09-14 11:18:26 -0500

    finished round 2

--- main.js
+++ main.js
@@ -12,3 +12,25 @@ const lengths = (arrayOfStrings) => {
 	return arrayOfNumbers;
 };
 lengths(["sam", "jack", "cathy"]);
+
+
+// Round 2
+
+// Write a Javascript function called transmogrifier This function should accept
+// three arguments, which you can assume will be numbers. Your function should
+// return the "transmogrified" result.
+// The transmogrified result of three numbers is the product (numbers multiplied
+// together) of the first two numbers, raised to the power (exponentially) of
+// the third number.
+
+const transmogrifier = (firstNumber, secondNumber, thirdNumber) => {
+	const originalTwoNumbers = firstNumber * secondNumber;
+	let transmogNumber = originalTwoNumbers;
+	for (i = 1; i <= thirdNumber; i++){
+		transmogNumber = transmogNumber * originalTwoNumbers;
+	}
+	return transmogNumber;
+};
+transmogrifier(5, 4, 3);
+transmogrifier(13, 12, 5);
+transmogrifier(42, 13, 7);
